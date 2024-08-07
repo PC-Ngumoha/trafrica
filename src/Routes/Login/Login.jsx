3cba569e8add60c18aba76a48fd351b0b86ad068
PC-Ngumoha <pcngumoha@gmail.com>
2024-08-07 10:49:14 +0100

    Update: create basic structure and style for Login & Register routes

--- src/Routes/Login/Login.jsx
+++ src/Routes/Login/Login.jsx
@@ -1,7 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faLock
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button/Button";
+import styles from './Login.module.css';
 
 const Login = () => {
   return (
-    <h1>This is the Login page</h1>
+    <main className={ styles.container }>
+      <form className={ styles.form }>
+        {/* Heading */}
+        <div className={ styles.heading }>
+          <h2>Welcome Back</h2>
+          <span>Enter your credentials to access your account</span>
+        </div>
+        {/* Input Areas */}
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faEnvelope } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="email"
+            name="email"
+            placeholder="Enter your email"
+            type="text"
+          />
+        </div>
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faLock } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="password"
+            name="password"
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+        {/* Call To Action */}
+        <div>
+          <span>Don't have an account ? <Link to='/register'>Register</Link>
+          </span>
+        </div>
+        {/* Submit */}
+        <Button className={ styles.button } message="Log In"/>
+      </form>
+    </main>
   );
 };
 
--- src/Routes/Register/Register.jsx
+++ src/Routes/Register/Register.jsx
@@ -1,7 +1,78 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import Button from '../../Components/Button/Button';
+import styles from './Register.module.css';
 
 const Register = () => {
   return (
-    <h1>This is the Register page</h1>
+    <main className={ styles.container }>
+      <form className={ styles.form }>
+        {/* Heading */}
+        <div className={ styles.heading }>
+          <h2>Join Us</h2>
+          <span>Enter your credentials to create an account</span>
+        </div>
+        {/* Input Areas */}
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faEnvelope } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="firstName"
+            name="firstName"
+            placeholder="Enter your first name"
+            type="text"
+          />
+        </div>
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faEnvelope } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="lastName"
+            name="lastName"
+            placeholder="Enter your last name"
+            type="text"
+          />
+        </div>
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faEnvelope } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="email"
+            name="email"
+            placeholder="Enter your email"
+            type="text"
+          />
+        </div>
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faLock } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="password"
+            name="password"
+            placeholder="Enter your password"
+            type="password"
+          />
+        </div>
+        <div className={ styles.inputArea }>
+          <FontAwesomeIcon icon={ faLock } className={ styles.icon }/>
+          <input
+            className=""
+            aria-label="confimPassword"
+            name="confirmPassword"
+            placeholder="Confirm password"
+            type="password"
+          />
+        </div>
+        {/* Call To Action */}
+        <div>
+          <span>Already have an account ? <Link to='/login'>Log In</Link>
+          </span>
+        </div>
+        {/* Submit */}
+        <Button className={ styles.button } message="Register"/>
+      </form>
+    </main>
   );
 };
 
